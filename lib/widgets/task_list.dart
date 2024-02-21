593877b2bc5717c29e73e902b9aaf1e6bfbb764f
FENET <productivefenetmuler@gmail.com>
2024-02-21 21:05:29 +0300

    Create task list

---
+++ lib/widgets/task_list.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:taskly/providers/tasks_provider.dart';
+import 'package:taskly/widgets/task_item.dart';
+
+class TaskList extends ConsumerWidget {
+  const TaskList({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tasks = ref.watch(tasksProvider);
+    return SizedBox(
+      height: 500,
+      width: 500,
+      child: ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: ((context, index) => Align(
+              alignment: Alignment.center,
+              child: TaskItem(task: tasks[index])))),
+    );
+  }
+}
